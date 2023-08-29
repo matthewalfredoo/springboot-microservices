74b81295d4066a2938b7472b6e3def95337b9c36
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-29 11:49:56 +0700

    87 - DepartmentService - Build Save Department REST API

---
+++ department-service/src/main/java/com/example/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.departmentservice.service.impl;
+
+import com.example.departmentservice.dto.DepartmentDto;
+import com.example.departmentservice.entity.Department;
+import com.example.departmentservice.repository.DepartmentRepository;
+import com.example.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor // will generate constructor to be used for constructor-based dependency injection
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+        // convert DepartmentDto to Department JPA Entity
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentDescription()
+        );
+
+        return savedDepartmentDto;
+    }
+
+}
