5a3beaf0de477927a59e7300439af9390aa65f19
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-29 13:52:23 +0700

    91 - EmployeeService - Build Save Employee REST API

---
+++ employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.example.employeeservice.controller;
+
+import com.example.employeeservice.dto.EmployeeDto;
+import com.example.employeeservice.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> saveEmployee(
+            @RequestBody
+            EmployeeDto employeeDto
+    ) {
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
