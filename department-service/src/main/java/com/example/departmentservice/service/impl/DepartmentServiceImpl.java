d7257448920e27daae119723b71fc2b0de1590d7
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-29 17:40:06 +0700

    Section 12 - Assignment 1 - Use Mapping Libraries (2)

--- department-service/src/main/java/com/example/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/example/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.departmentservice.entity.Department;
 import com.example.departmentservice.repository.DepartmentRepository;
 import com.example.departmentservice.service.DepartmentService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,24 +14,28 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private DepartmentRepository departmentRepository;
 
+    private ModelMapper modelMapper;
+
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
         // convert DepartmentDto to Department JPA Entity
-        Department department = new Department(
+        /*Department department = new Department(
                 departmentDto.getId(),
                 departmentDto.getDepartmentName(),
                 departmentDto.getDepartmentDescription(),
                 departmentDto.getDepartmentCode()
-        );
+        );*/
+        Department department = modelMapper.map(departmentDto, Department.class);
 
         Department savedDepartment = departmentRepository.save(department);
 
-        DepartmentDto savedDepartmentDto = new DepartmentDto(
+        /*DepartmentDto savedDepartmentDto = new DepartmentDto(
                 savedDepartment.getId(),
                 savedDepartment.getDepartmentName(),
                 savedDepartment.getDepartmentDescription(),
                 savedDepartment.getDepartmentDescription()
-        );
+        );*/
+        DepartmentDto savedDepartmentDto = modelMapper.map(savedDepartment, DepartmentDto.class);
 
         return savedDepartmentDto;
     }
@@ -39,12 +44,13 @@ public class DepartmentServiceImpl implements DepartmentService {
     public DepartmentDto getDepartmentByCode(String departmentCode) {
         Department department = departmentRepository.findByDepartmentCode(departmentCode);
 
-        DepartmentDto departmentDto = new DepartmentDto(
+        /*DepartmentDto departmentDto = new DepartmentDto(
                 department.getId(),
                 department.getDepartmentName(),
                 department.getDepartmentDescription(),
                 department.getDepartmentCode()
-        );
+        );*/
+        DepartmentDto departmentDto = modelMapper.map(department, DepartmentDto.class);
 
         return departmentDto;
     }
