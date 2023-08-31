89b2554bcc6578d6f1f3570c7cbd81f46b0e7172
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-31 14:31:03 +0700

    148 - Create Organization JPA Entity and Spring Data JPA Repository

---
+++ organization-service/src/main/java/com/example/organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package com.example.organizationservice.repository;
+
+import com.example.organizationservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
