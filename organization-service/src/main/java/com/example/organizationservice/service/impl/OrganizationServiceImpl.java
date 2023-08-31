5e590d67e179e6c763d1d359fa399d38560bd683
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-08-31 14:48:18 +0700

    150 - Build Save Organization REST API

---
+++ organization-service/src/main/java/com/example/organizationservice/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.organizationservice.service.impl;
+
+import com.example.organizationservice.dto.OrganizationDto;
+import com.example.organizationservice.entity.Organization;
+import com.example.organizationservice.mapper.OrganizationMapper;
+import com.example.organizationservice.repository.OrganizationRepository;
+import com.example.organizationservice.service.OrganizationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrganizationServiceImpl implements OrganizationService {
+
+    private OrganizationRepository organizationRepository;
+
+    @Override
+    public OrganizationDto saveOrganization(OrganizationDto organizationDto) {
+        // convert OrganizationDto to Organization JPA Entity
+        Organization organization = OrganizationMapper.mapToOrganization(organizationDto);
+
+        Organization savedOrganization = organizationRepository.save(organization);
+
+        return OrganizationMapper.mapToOrganizationDto(savedOrganization);
+    }
+
+}
